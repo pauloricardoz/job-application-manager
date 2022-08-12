d9d61c32569e5b7b92061267556b17705455e4fc
pauloricardoz <trybe.przt@gmail.com>
2022-08-12 11:57:41 -0300

    FEAT: outra forma de fazer test de controller

--- tests/unit/controllers/empresaController.test.js
+++ tests/unit/controllers/empresaController.test.js
@@ -85,10 +85,16 @@ describe('GET ALL outra forma de testar', () => {
     };
 
     const response = {
-      json: (obj) => { result.body = obj; return null; },
-      status: (num) => { result.status = num; return response; },
+      json: (obj) => {
+        result.body = obj;
+        return null; 
+        },
+      status: (num) => {
+      result.status = num; 
+      return response; 
+      },
     };
-    
+
     const spyJson = Sinon.spy(response, 'json');
     const spyStatus = Sinon.spy(response, 'status');
     await controller(request, response);
@@ -124,38 +130,32 @@ describe('GET ALL outra forma de testar', () => {
     });
   });
   describe('Caso OK com BD com dados', () => {
-    let request = {};
-    let response = {};
     beforeEach(async function () {
-      request = {};
-      response = {};
-  
-      response.status = Sinon.stub().returns(response);
-      response.json = Sinon.stub().returns();
       const resultExecute = [{ name: 'teste Inc.', id: 99 }];
       Sinon.stub(empresaService, 'getAll').resolves(resultExecute);
-  
-      await empresaController.getAll(request, response);
     });
     afterEach(function () {
       Sinon.restore();
     });
     it('retorna status 200', async function () {
       // AAA - ARRANGE - ACT - ASSERT
-      expect(response.status.calledWith(200)).to.be.equal(true);
+      const resposta = await testMyController(empresaController.getAll);
+      expect(resposta.status).to.be.equal(200);
     });
     it('retorna status json com array vazio', async function () {
       // AAA - ARRANGE - ACT - ASSERT
-      expect(response.json.calledWith([{ name: 'teste Inc.', id: 99 }]))
-        .to.be.equal(true);
+      const resposta = await testMyController(empresaController.getAll);
+      expect(resposta.body).to.be.deep.equal([{ name: 'teste Inc.', id: 99 }]);
     });
     it('status chamado uma unica vez', async function () {
       // AAA - ARRANGE - ACT - ASSERT
-      expect(response.status.calledOnce).to.be.true;
+      const resposta = await testMyController(empresaController.getAll);
+      expect(resposta.spies.status.calledOnce).to.be.equal(true);
     });
     it('json chamado uma unica vez', async function () {
       // AAA - ARRANGE - ACT - ASSERT
-      expect(response.json.calledOnce).to.be.true;
+      const resposta = await testMyController(empresaController.getAll);
+      expect(resposta.spies.json.calledOnce).to.be.equal(true);
     });
   });
 });
