ec0e56d2fbda3dc3dd1076f242217b9fccd3ee9c
pauloricardoz <trybe.przt@gmail.com>
2022-08-13 18:37:30 -0300

    test: extra describe layer

--- tests/unit/models/empresaModel.test.js
+++ tests/unit/models/empresaModel.test.js
@@ -5,50 +5,94 @@ const connection = require('../../../models/connection');
 
 const empresaModel = require('../../../models/empresaModel');
 
-describe('GET ALL', () => {
-  describe('Caso OK', () => {
-    // before(()=>{
-    //   const resultExecute = []
-    //   Sinon.stub(connection, 'execute').resolves([resultExecute]);
-    // })
-    afterEach(()=>{
-      // connection.execute.restore();
-      Sinon.restore();
-    })
-    it('retorna array', async function () {
-      // AAA - ARRANGE - ACT - ASSERT
-      const resultExecute = []
-      Sinon.stub(connection, 'execute').resolves([resultExecute]);
+describe.only('MODEL EMPRESA', () => {
+  describe('GET ALL', () => {
+    describe('Caso OK', () => {
+      afterEach(function () {
+        Sinon.restore();
+      });
+      it('retorna array', async function () {
+        // AAA - ARRANGE - ACT - ASSERT
+        const resultExecute = [];
+        Sinon.stub(connection, 'execute').resolves([resultExecute]);
 
-      const resultado = await empresaModel.getAll();
+        const resultado = await empresaModel.getAll();
 
-      expect(resultado).to.be.an('array');
+        expect(resultado).to.be.an('array');
+      });
+      it('retorna array vazio', async function () {
+        // AAA - ARRANGE - ACT - ASSERT
+        const resultExecute = [];
+        Sinon.stub(connection, 'execute').resolves([resultExecute]);
+
+        const resultado = await empresaModel.getAll();
+
+        expect(resultado).to.be.empty;
+      });
+      it('Retorna array cheio', async function () {
+        const resultExecute = [{ id: 1, name: 'teste' }];
+        Sinon.stub(connection, 'execute').resolves([resultExecute]);
+
+        const resultado = await empresaModel.getAll();
+
+        expect(resultado).to.be.not.empty;
+      });
+      it('Retorna array contenha objetos ', async function () {
+        const resultExecute = [{ id: 1, name: 'teste' }];
+        Sinon.stub(connection, 'execute').resolves([resultExecute]);
+
+        const [resultado] = await empresaModel.getAll();
+
+        expect(resultado).to.be.an('object');
+        expect(resultado).to.all.keys('name', 'id');
+      });
     });
-    it('retorna array vazio', async function () {
-      // AAA - ARRANGE - ACT - ASSERT
-      const resultExecute = []
-      Sinon.stub(connection, 'execute').resolves([resultExecute]);
+  });
+  describe.only('GET BY ID', () => {
+    describe('Caso OK', () => {
+      // before(()=>{
+      //   const resultExecute = []
+      //   Sinon.stub(connection, 'execute').resolves([resultExecute]);
+      // })
+      afterEach(function () {
+        // connection.execute.restore();
+        Sinon.restore();
+      });
+      it('retorna array', async function () {
+        // AAA - ARRANGE - ACT - ASSERT
+        const resultExecute = [];
+        Sinon.stub(connection, 'execute').resolves([resultExecute]);
 
-      const resultado = await empresaModel.getAll();
+        const resultado = await empresaModel.getAll();
 
-      expect(resultado).to.be.empty;
+        expect(resultado).to.be.an('array');
+      });
+      it('retorna array vazio', async function () {
+        // AAA - ARRANGE - ACT - ASSERT
+        const resultExecute = [];
+        Sinon.stub(connection, 'execute').resolves([resultExecute]);
+
+        const resultado = await empresaModel.getAll();
+
+        expect(resultado).to.be.empty;
+      });
+      it('Retorna array cheio', async function () {
+        const resultExecute = [{ id: 1, name: 'teste' }];
+        Sinon.stub(connection, 'execute').resolves([resultExecute]);
+
+        const resultado = await empresaModel.getAll();
+
+        expect(resultado).to.be.not.empty;
+      });
+      it('Retorna array contenha objetos ', async function () {
+        const resultExecute = [{ id: 1, name: 'teste' }];
+        Sinon.stub(connection, 'execute').resolves([resultExecute]);
+
+        const [resultado] = await empresaModel.getAll();
+
+        expect(resultado).to.be.an('object');
+        expect(resultado).to.all.keys('name', 'id');
+      });
     });
-    it('Retorna array cheio', async()=>{
-      const resultExecute = [{id: 1, name: 'teste'}]
-      Sinon.stub(connection, 'execute').resolves([resultExecute]);
-
-      const resultado = await empresaModel.getAll();
-
-      expect(resultado).to.be.not.empty;
-    })
-    it('Retorna array contenha objetos ', async()=>{
-      const resultExecute = [{id: 1, name: 'teste'}]
-      Sinon.stub(connection, 'execute').resolves([resultExecute]);
-
-      const [resultado] = await empresaModel.getAll();
-
-      expect(resultado).to.be.an('object');
-      expect(resultado).to.all.keys('name', 'id')
-    })
   });
 });
