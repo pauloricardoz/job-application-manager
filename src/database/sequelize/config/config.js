b703f915ddd05dd36a4b90ed51110a66599b73f8
pauloricardoz <trybe.przt@gmail.com>
2022-09-22 23:35:56 -0300

    feat: sequelize config 3 environment

---
+++ src/database/sequelize/config/config.js
@@ -0,0 +1,34 @@
+require('dotenv/config');
+
+const {
+  DB_HOST: host,
+  DB_PORT: dbPort,
+  DB_USER: username,
+  DB_PASSWORD: password,
+  DB_DATA_BASE: database,
+  DIALECT: dialect = 'mysql',
+} = process.env;
+
+module.exports = {
+  development: {
+    username,
+    password,
+    database,
+    host,
+    dialect,
+  },
+  test: {
+    username,
+    password,
+    database:database + "-test",
+    host,
+    dialect,
+  },
+  production: {
+    username,
+    password,
+    database:database + "-prod",
+    host,
+    dialect,
+  },
+};
